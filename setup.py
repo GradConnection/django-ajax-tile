a015fa0b27bec0b97643fd955749aa4c1e65d743
Eric Man <meric.au@gmail.com>
2012-09-12 14:10:40 +1000

    Refactor directory

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-version_tuple = __import__('tile').VERSION
-if len(version_tuple) == 3:
-    version = "%d.%d_%s" % version_tuple
-else:
-    version = "%d.%d" % version_tuple[:2]
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+from setuptools import find_packages, setup
 
 setup(name = "tile",
       author = "Eric Man",
       url = "http://github.com/GradConnection/django-ajax-tile",
-      version = version,
-      packages = ['tile',],
-      package_dir = {'': ''},
-      package_data = {'tile': ['static/tile/*',],},
-      # distutils complain about these, anyone know an easy way to silence it?
+      version = "0.0",
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+      include_package_data = True,
       zip_safe = True,
+      install_requires = ['setuptools'],
 )
 
